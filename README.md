57afaf5e43e918d357a748db6c36c6046b78817e
rupalee10 <lokhande.rupalee10@gmail.com>
2020-04-15 14:42:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-38t4q5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-38t4q5)
---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {FormControl} from '@angular/forms'
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+  username =new FormControl();
+  password=new FormControl();
+}
